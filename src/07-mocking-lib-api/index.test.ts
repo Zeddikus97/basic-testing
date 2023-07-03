5d9653ac6ebc9a8cd0f269bd0cfd4f8ca0c1e4cc
Pavel <opatriarkh@gmail.com>
2023-07-04 00:28:37 +0300

    test: added mocking axious + snapshot

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,59 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
+const USERS_PATH = 'users';
 
 describe('throttledGetDataFromApi', () => {
-  test('should create instance with provided base url', async () => {
-    // Write your test here
+  beforeAll(() => {
+    jest.useFakeTimers();
   });
 
-  test('should perform request to correct provided url', async () => {
-    // Write your test here
+  beforeEach(() => {
+    //spyGet?.mockClear();
+
   });
 
-  test('should return response data', async () => {
-    // Write your test here
+  afterAll(() => {
+    jest.useRealTimers();
   });
+
+  test(
+    'should create instance with provided base url',
+    async () => {
+      const spyCreate = jest.spyOn(axios, 'create');
+      await throttledGetDataFromApi(USERS_PATH);
+      jest.runAllTimers();
+      expect(spyCreate).toHaveBeenCalledWith({ baseURL: BASE_URL });
+    }
+  );
+
+  test(
+    'should perform request to correct provided url',
+    async () => {
+      const spyGet = jest
+      .spyOn(axios.Axios.prototype, 'get')
+      .mockImplementation(async () => ({ data: {} }));
+      await throttledGetDataFromApi(USERS_PATH);
+      jest.runAllTimers();
+      expect(spyGet).toBeCalledWith(USERS_PATH);
+    }
+  );
+
+  test(
+    'should return response data',
+    async () => {
+      let mockingData = [
+        { id: '1', name: 'test' },
+        { id: '2', name: 'denis' }
+      ];
+      const spyGet = jest
+      .spyOn(axios.Axios.prototype, 'get')
+      .mockImplementation(async () => ({ data: mockingData }));
+      
+      const users = await throttledGetDataFromApi(USERS_PATH);
+      expect(spyGet).toBeCalledWith(USERS_PATH);
+      expect(users).toEqual(mockingData);
+    }
+  );
 });
